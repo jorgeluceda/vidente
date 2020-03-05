4705337e19d8957a808856987296e9a4cbebece5
Jorge Euceda <jeuce004@fiu.edu>
2020-03-05 14:54:49 -0800

    delete previous barcode.js

--- public/javascripts/barcode.js
+++
@@ -1,9 +0,0 @@
-module.exports = function() {
-    JsBarcode("#barcode", "1234", {
-        format: "CODE39",
-        lineColor: "#0aa",
-        width: 3,
-        height: 50,
-        displayValue: false
-    });
-}
