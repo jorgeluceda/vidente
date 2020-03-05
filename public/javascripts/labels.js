4a6dc63ce7ebdb2eb38080ba43a97265f68e06b1
Jorge Euceda <jeuce004@fiu.edu>
2020-03-05 15:14:50 -0800

    use jquery for label removal animation

--- public/javascripts/labels.js
+++ public/javascripts/labels.js
@@ -1,5 +1,5 @@
 $('.close').on('click', function(e){
     e.stopPropagation();  
-        var $target = $(this).parents('.col-sm-3');
-        $target.hide('slow', function(){ $target.remove(); });
+    var $target = $(this).parents('.card');
+    $target.hide('fast', function(){ $target.remove(); });
 });
