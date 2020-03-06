adbd21255594b52f4fdb551c5e96ee52a90efc11
Jorge Euceda <jeuce004@fiu.edu>
2020-03-06 10:05:08 -0800

    add support for touch on jquery .close methods

--- public/javascripts/labels.js
+++ public/javascripts/labels.js
@@ -1,4 +1,4 @@
-$('.close').on('click', function(e){
+$('.close').on('click touchstart', function(e){
     e.stopPropagation();  
     var $target = $(this).parents('.card');
     $target.hide('fast', function(){ $target.remove(); });
