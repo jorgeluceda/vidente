7faf2e4f046de5ebd7f8af681bce123388553eff
Jorge Euceda <jeuce004@fiu.edu>
2020-03-12 22:31:45 -0700

    implement vidente-app's Menu on React

--- app_server/vidente-app/src/components/app/App.js
+++ app_server/vidente-app/src/components/app/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from '../images/logo.svg';
+
+import Header from '../header/header';
+import MenuHeader from "../menu-header/MenuHeader";
+import Menu from "../menu/Menu";
 import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid-page">
+      <Header/>
+      <MenuHeader/>
+      <Menu/>
     </div>
+
   );
 }
 
---
+++ app_server/vidente-app/src/components/menu-header/MenuHeader.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function MenuHeader() {
+  return(
+    <div className="menu-header">
+
+      <div className="mh-contents">
+        <h6>
+          Groups&nbsp;&nbsp;
+        </h6>
+        <button href="" onTouchStart="">
+          <span> New Group </span>
+        </button>
+      </div>
+
+      <div className="border-bottom"/>
+
+    </div>
+  );
+}
+
+export default MenuHeader;
+
