1069985421186a2d5b24489e0311890a2ae06305
Jorge Euceda <jeuce004@fiu.edu>
2020-04-22 23:18:42 -0400

    improve spacing and padding in GroupsModal

--- app_server/vidente-app/src/components/MainPage/Groups/GroupsModal/GroupsModal.js
+++ app_server/vidente-app/src/components/MainPage/Groups/GroupsModal/GroupsModal.js
@@ -49,19 +49,19 @@ function GroupsModal(props) {
                 }
             }}>
                 {selectedInput[1] == true &&
-                    <a onClick={() => setSelectedInput([0, false])}>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1.5em">
+                    <button className={styles.group_button}onClick={() => setSelectedInput([0, false])}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="2em">
                             <path fill="#464646" d="M12,2A10,10,0,1,0,22,12,10,10,0,0,0,12,2Zm3.21,11.79a1,1,0,0,1,0,1.42,1,1,0,0,1-1.42,0L12,13.41l-1.79,1.8a1,1,0,0,1-1.42,0,1,1,0,0,1,0-1.42L10.59,12l-1.8-1.79a1,1,0,0,1,1.42-1.42L12,10.59l1.79-1.8a1,1,0,0,1,1.42,1.42L13.41,12Z"/>
                         </svg>
-                    </a>
+                    </button>
                 }
 
                 <input className={styles.new_group_input} autoFocus={selectedInput[1] === true}
                    name="name" placeholder="Create new group" autoComplete="off" onClick={() => {setSelectedInput([-1, true])}}/>
-                <button className={styles.new_group_check}>
+                <button className={styles.group_button}>
                     {/* Check button for Create new group*/}
                     {selectedInput[1] === true &&
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1.5em">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="2em">
                             <path fill="#464646" d="M12,2A10,10,0,1,0,22,12,10,10,0,0,0,12,2Zm4.71,7.71-5,5a1,1,0,0,1-1.42,0l-2-2a1,1,0,0,1,1.42-1.42L11,12.59l4.29-4.3a1,1,0,0,1,1.42,1.42Z"/>
                         </svg>
                     }
@@ -72,13 +72,13 @@ function GroupsModal(props) {
                 poppedGroup.map((group, i) => (
                             <div>
                                 <p key={i} className={`no-select ${styles.modal_p}`}>
-                                    <a onClick={() => {
+                                    <button className={styles.group_button} onClick={() => {
                                         props.deleteGroup(group._id);
                                     }}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1.25em">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1.75em">
                                             <path fill="#464646" d="M21 6a1 1 0 0 1-1 1H4A1 1 0 0 1 4 5H9V4.5A1.5 1.5 0 0 1 10.5 3h3A1.5 1.5 0 0 1 15 4.5V5h5A1 1 0 0 1 21 6zM5.5 9v9.5A2.5 2.5 0 0 0 8 21h8a2.5 2.5 0 0 0 2.5-2.5V9zM11 17a1 1 0 0 1-2 0V13a1 1 0 0 1 2 0zm4 0a1 1 0 0 1-2 0V13a1 1 0 0 1 2 0z"/>
                                         </svg>
-                                    </a>
+                                    </button>
 
                                     <input className={styles.edit_group_input} autoFocus={selectedInput[0] === (i) && selectedInput[1] !== true}
                                            name="name" value={group.name} autoComplete="off" onClick={() => {
@@ -87,25 +87,26 @@ function GroupsModal(props) {
 
                                     {selectedInput[0] === (i) && selectedInput[1] !== true
                                         ?
-                                        <a>
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1.25em">
-                                                <path fill="#464646" d="M12,2A10,10,0,1,0,22,12,10,10,0,0,0,12,2Zm4.71,7.71-5,5a1,1,0,0,1-1.42,0l-2-2a1,1,0,0,1,1.42-1.42L11,12.59l4.29-4.3a1,1,0,0,1,1.42,1.42Z"/>
+                                        <button className={styles.group_button}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1.75em">
+                                                    <path fill="#464646" d="M12,2A10,10,0,1,0,22,12,10,10,0,0,0,12,2Zm4.71,7.71-5,5a1,1,0,0,1-1.42,0l-2-2a1,1,0,0,1,1.42-1.42L11,12.59l4.29-4.3a1,1,0,0,1,1.42,1.42Z"/>
                                             </svg>
-                                        </a>
+                                        </button>
                                         :
-                                        <a>
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1.25em">
+                                        <button className={styles.group_button}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1.75em">
                                                 <path fill="#464646" d="M17,22H5a3,3,0,0,1-3-3V7A3,3,0,0,1,5,4H9A1,1,0,0,1,9,6H5A1,1,0,0,0,4,7V19a1,1,0,0,0,1,1H17a1,1,0,0,0,1-1V15a1,1,0,0,1,2,0v4A3,3,0,0,1,17,22Z"/>
                                                 <path fill="#464646" d="M14.6 5.87l-4.95 5a.41.41 0 0 0-.13.23l-1 3.82a.48.48 0 0 0 .13.48A.47.47 0 0 0 9 15.5a.32.32 0 0 0 .13 0l3.82-1a.41.41 0 0 0 .23-.13L18.13 9.4zM21 4.45L19.55 3a1.52 1.52 0 0 0-2.13 0L16 4.45 19.55 8 21 6.58A1.52 1.52 0 0 0 21 4.45z"/>
                                             </svg>
-                                        ️</a>
+                                        </button>
                                         }
                                 </p>
                             </div>
                 ))
             }
 
-            <button onClick={props.closeModal} style={{alignSelf: "flex-end"}}>Done</button>
+            <div className={styles.modal_border}></div>
+            <button className={styles.done_button} onClick={props.closeModal}>Done</button>
         </Modal>
 
     );
