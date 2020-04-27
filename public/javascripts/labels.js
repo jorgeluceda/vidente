6a39bee28ee4591951fe9c5b012e96472de3e5d8
Jorge Euceda <jeuce004@fiu.edu>
2020-04-27 06:07:13 -0400

    delete previous use of jquery and other javascript files

--- public/javascripts/labels.js
+++
@@ -1,5 +0,0 @@
-$('.close').on('click touchstart', function(e){
-    e.stopPropagation();  
-    var $target = $(this).parents('.card');
-    $target.hide('fast', function(){ $target.remove(); });
-});
